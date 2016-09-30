aa654b2da1472865c15a8ef645894ffdbef1056a
Sönke Hahn <soenkehahn@gmail.com>
2016-09-30 19:52:29 -0400

    wip

--- src/Servant/API/Check.hs
+++ src/Servant/API/Check.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -14,27 +15,27 @@ import           Data.Proxy
 import           GHC.TypeLits
 import           Servant.API
 
-type IsValid api = RunEither (ValidateRequestBodies api)
+type IsValid api = (RunEither (CheckApi api) :: Constraint)
 
-isValid :: (IsValid api) => Proxy api -> ()
-isValid Proxy = ()
+data ValidApiWitness api where
+  ValidApiWitness :: IsValid api => ValidApiWitness api
 
-validateRequestBodies :: Proxy api -> Proxy (ValidateRequestBodies api)
+validateRequestBodies :: Proxy api -> Proxy (CheckApi api)
 validateRequestBodies Proxy = Proxy
 
-type family ValidateRequestBodies (api :: *) :: Either ErrorMessage () where
-  ValidateRequestBodies api = ValidateRequestBodiesH NoReqBody api
+type family CheckApi (api :: *) :: Either ErrorMessage () where
+  CheckApi api = CheckApiH NoReqBody api
 
 data HasReqBody api
   = YesReqBody (api :: *)
   | NoReqBody
 
-type family ValidateRequestBodiesH
+type family CheckApiH
   (hasReqBody :: HasReqBody endpoint)
   (api :: *)
     :: Either ErrorMessage () where
 
-  ValidateRequestBodiesH
+  CheckApiH
     (YesReqBody reqBodyEndpoint)
     (Verb (GET :: StdMethod) (status :: Nat) (contentTypes :: [*]) (result :: *)) =
       Left (
@@ -42,20 +43,39 @@ type family ValidateRequestBodiesH
         ShowType reqBodyEndpoint :<>:
         'Text ")")
 
-  ValidateRequestBodiesH
+  CheckApiH
     hasReqBody
     (Verb (method :: StdMethod) (status :: Nat) (contentTypes :: [*]) (result :: *)) =
       Right '()
 
-  ValidateRequestBodiesH
-    hasReqBody
-    (QueryParam (name :: Symbol) (p :: *) :> api)
-      = ValidateRequestBodiesH hasReqBody api
-  ValidateRequestBodiesH hasReqBody (ReqBody (contentTypes :: [*]) (body :: *) :> api) =
-    ValidateRequestBodiesH (YesReqBody (ReqBody contentTypes body :> api)) api
+  CheckApiH hasReqBody (combinator :> api) =
+    IsCombinator combinator >>
+    CheckApiH hasReqBody api
 
-  ValidateRequestBodiesH hasReqBody api =
+  CheckApiH hasReqBody api =
     Left ('Text "invalid api type: " ':<>: 'ShowType api)
+
+type family IsCombinator (combinator :: *) :: Either ErrorMessage () where
+  IsCombinator (QueryParam (name :: Symbol) (p :: *))
+    = Right '()
+  IsCombinator (ReqBody (contentTypes :: [*]) (body :: *))
+    = Right '()
+  IsCombinator combinator = IsCombinatorOpen combinator
+  {- IsCombinator combinator
+    = Left ('Text "invalid combinator: " ':<>: 'ShowType combinator) -}
+
+type family IsCombinatorOpen (combinator :: *) :: Either ErrorMessage ()
+
+type instance IsCombinatorOpen api =
+  Left ('Text "invalid combinator: " ':<>: 'ShowType api)
+
+type family (>>=) (a :: Either e k) (b :: k -> Either e ()) :: Either e () where
+  Left err >>= next = Left err
+  Right result >>= next = next result
+
+type family (>>) (a :: Either e ()) (b :: Either e ()) :: Either e () where
+  Left err >> next = Left err
+  Right '() >> next = next
 
 type family RunEither (action :: Either ErrorMessage ()) :: Constraint where
   RunEither ('Right '()) = (() :: Constraint)
--- test/Servant/API/CheckSpec.hs
+++ test/Servant/API/CheckSpec.hs
@@ -21,79 +21,96 @@ import           Test.Hspec
 
 import           Servant.API.Check
 
+data Custom
+
+type instance IsCombinatorOpen Custom = Right '()
+
+type Get' = Get '[JSON]
+
 spec :: Spec
 spec = do
   describe "validateRequestBodies" $ do
+    it "works for valid apis" $ do
+      validateRequestBodies (Proxy :: Proxy (Get' Int))
+        `shouldHaveDemoted` Right ()
+
+    it "" $ do
+      validateRequestBodies (Proxy :: Proxy (() :> Get' Int))
+        `shouldHaveDemoted` Left "invalid combinator: ()"
+
+    it "allows to add custom combinators" $ do
+      validateRequestBodies (Proxy :: Proxy (Custom :> Get' Int))
+        `shouldHaveDemoted` Right ()
+
     it "complains about GET endpoints with request body" $ do
-      validateRequestBodies (Proxy :: Proxy (ReqBody '[JSON] Int :> Get '[JSON] Int))
+      pending
+      validateRequestBodies (Proxy :: Proxy (ReqBody '[JSON] Int :> Get' Int))
         `shouldHaveDemoted`
         Left "GET endpoints shouldn't have request bodies. (:> * * (ReqBody * (': * JSON '[]) Int) (Verb StdMethod * 'GET 200 (': * JSON '[]) Int))"
-
-    it "works for valid apis" $ do
-      validateRequestBodies (Proxy :: Proxy (QueryParam "foo" Int :> Get '[JSON] Int))
-        `shouldHaveDemoted` Right ()
 
     it "reports invalid apis" $ do
       validateRequestBodies (Proxy :: Proxy ())
         `shouldHaveDemoted` Left "invalid api type: ()"
 
     it "" $ do
-      let () = isValid (Proxy :: Proxy (QueryParam "foo" Int :> Get '[JSON] Int))
-      return () :: IO ()
+      let validProxy :: ValidApiWitness (QueryParam "foo" Int :> Get' Int)
+          validProxy = ValidApiWitness
+      True
 
     it "" $ do
       pending
-      -- print $ validate (Proxy :: Proxy (Get '[JSON] Int))
+      -- print $ validate (Proxy :: Proxy (Get' Int))
 
+  describe "demoting (fixme)" $ do
     it "" $ do
       let  p = Proxy :: Proxy "huhu"
-      lower p `shouldBe` ("huhu" :: String)
+      p `shouldHaveDemoted` ("huhu" :: String)
       let  p = Proxy :: Proxy ('() :: ())
-      lower p `shouldBe` ()
+      p `shouldHaveDemoted` ()
       let  p = Proxy :: Proxy ('Right '() :: Either () ())
-      lower p `shouldBe` Right ()
+      p `shouldHaveDemoted` Right ()
       let  p = Proxy :: Proxy ('Left '() :: Either () ())
-      lower p `shouldBe` Left ()
+      p `shouldHaveDemoted` Left ()
       let  p = Proxy :: Proxy ('Text "foo")
-      lower p `shouldBe` "foo"
+      p `shouldHaveDemoted` "foo"
 
 shouldHaveType :: (?loc :: CallStack, Typeable a) => Proxy (a :: k) -> String -> IO ()
 shouldHaveType p expected =
   show (typeRep p) `shouldBe` expected
 
-shouldHaveDemoted :: (Demote k a, Eq (Term k), Show (Term k)) =>
+shouldHaveDemoted :: (?loc :: CallStack, Typeable a, Demote k a, Eq (Term k), Show (Term k)) =>
   Proxy (a :: k) -> Term k -> IO ()
 shouldHaveDemoted proxy expected =
-  lower proxy `shouldBe` expected
+  demote proxy `shouldBe` expected
 
 class Demote (k :: Type) (t :: k) where
   type Term k :: Type
-  lower :: Proxy (t :: k) -> Term k
+  demote :: Proxy (t :: k) -> Term k
 
 instance KnownSymbol t => Demote Symbol t where
   type Term Symbol = String
-  lower proxy = symbolVal proxy
+  demote proxy = symbolVal proxy
 
 instance Demote () '() where
   type Term () = ()
-  lower Proxy = ()
+  demote Proxy = ()
 
 instance Demote k a => Demote (Either k l) (Left (a :: k)) where
   type Term (Either k l) = Either (Term k) (Term l)
-  lower Proxy = Left $ lower (Proxy :: Proxy a)
+  demote Proxy = Left $ demote (Proxy :: Proxy a)
 
 instance Demote l a => Demote (Either k l) (Right (a :: l)) where
   type Term (Either k l) = Either (Term k) (Term l)
-  lower Proxy = Right $ lower (Proxy :: Proxy a)
+  demote Proxy = Right $ demote (Proxy :: Proxy a)
 
 instance KnownSymbol symbol => Demote ErrorMessage (Text symbol) where
   type Term ErrorMessage = String
-  lower Proxy = lower (Proxy :: Proxy symbol)
+  demote Proxy = demote (Proxy :: Proxy symbol)
 
 instance (Demote ErrorMessage a, Demote ErrorMessage b) => Demote ErrorMessage (a :<>: b) where
   type Term ErrorMessage = String
-  lower Proxy = lower (Proxy :: Proxy a) ++ lower (Proxy :: Proxy b)
+  demote Proxy = demote (Proxy :: Proxy a) ++ demote (Proxy :: Proxy b)
 
 instance Typeable t => Demote ErrorMessage (ShowType t) where
   type Term ErrorMessage = String
-  lower Proxy = show $ typeRep (Proxy :: Proxy t)
+  demote Proxy = show $ typeRep (Proxy :: Proxy t)
